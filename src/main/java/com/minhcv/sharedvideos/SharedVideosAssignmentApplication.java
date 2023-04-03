62f3f5d2c794923c3678568243965fb997add005
minh.chu <minh.chu@playstudios-il.com>
2023-04-03 23:27:44 +0700

    Initialize project

---
+++ src/main/java/com/minhcv/sharedvideos/SharedVideosAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.minhcv.sharedvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedVideosAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedVideosAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minhcv/sharedvideos/SharedVideosAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhcv.sharedvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedVideosAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
