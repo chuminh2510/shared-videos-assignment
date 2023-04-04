53ef9f904a936576a82099645b2d1d7359bf6939
minh.chu <minh.chu@playstudios-il.com>
2023-04-04 23:47:39 +0700

    Video info template

--- src/main/java/com/minhcv/sharedvideos/config/SecurityConfig.java
+++ src/main/java/com/minhcv/sharedvideos/config/SecurityConfig.java
@@ -25,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
