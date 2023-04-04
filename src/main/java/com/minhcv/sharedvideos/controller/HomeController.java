efb1d684e0b0a2e3e18285cc14b466b4f182138b
minh.chu <minh.chu@playstudios-il.com>
2023-04-04 22:14:28 +0700

    Home page initialization

--- src/main/java/com/minhcv/sharedvideos/controller/HomeController.java
+++ src/main/java/com/minhcv/sharedvideos/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.minhcv.sharedvideos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    String home(){
-        return "index";
+
+    @GetMapping("")
+    String home(Model model) {
+        return "home";
     }
 }
